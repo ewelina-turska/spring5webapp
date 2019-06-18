abdafc1ec9a07aa2403a6a2a6ed554eb189632f1
ewelina-turska <ewelinaturska27@gmail.com>
2019-06-18 15:15:19 +0200

    publisher class added

---
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,39 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    @OneToOne(mappedBy = "publisher")
+    private Book book;
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String address, Book book) {
+        this.name = name;
+        this.address = address;
+        this.book = book;
+    }
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
